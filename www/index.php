e87700f5b3b25a99839b19bfac735c413d71d0b9
luizhakan <luizhakan2@protonmail.com>
2023-02-13 20:09:24 -0300

    Docker + PHP Neste repositório eu treinei como usar o docker e criei um container com PHP pra rodar num servidor apache.

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<h1>Lista de Usuários</h1>
+
+<?php
+
+$pdo = new PDO("mysql: dbname=meudb;host=db", "luizhakan", "1234");
+$sql = $pdo->query("SELECT * FROM usuarios");
+
+if ($sql) {
+    $usuarios = $sql->fetchAll();
+    echo "<ul>";
+    foreach ($usuarios as $usuario) {
+        echo "<li>" . $usuario['nome'] . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "Não há usuários cadastrados!";
+}
